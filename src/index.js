230db6a025a41789c71d64e3cb02133ef2a6db70
Brandon Wie <brandonwie.cs@gmail.com>
2022-01-13 18:39:10 +0900

    setup react app manually

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+const App = () => <div>Hello World</div>;
+
+ReactDOM.render(<App />, root, () => {
+  return console.log('React app is rendered.');
+});
